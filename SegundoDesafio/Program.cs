e735142f388bbc622c84082510f47cf6605fb4e8
Mari Afonso <marianaafonso007@gmail.com>
2023-12-10 22:42:47 -0300

    Resolver o segundo desafio

---
+++ SegundoDesafio/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Seja bem vindo. ao projeto de SOMA!");
+
+Console.WriteLine("Digite o primeiro número a ser somado!");
+var numero1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número a ser somado!");
+var numero2 = Convert.ToInt32(Console.ReadLine());
+
+var resultado = SomandoNumeros();
+
+Console.WriteLine("A soma deu:");
+Console.WriteLine(resultado);
+
+int SomandoNumeros()
+{
+    var soma = numero1 + numero2;
+    return soma;
+}
